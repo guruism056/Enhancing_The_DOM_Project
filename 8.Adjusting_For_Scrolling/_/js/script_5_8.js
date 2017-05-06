89a94261fe102fb9149bc95813725c7f19733276
guruism056 <guruism056@gmail.com>
2017-05-06 12:12:24 +0530

    Adjusting For Scrolling

---
+++ 8.Adjusting_For_Scrolling/_/js/script_5_8.js
@@ -0,0 +1,93 @@
+(function()
+{
+	var myNode = document.querySelector("#artlist .pixgrid ul");
+	var body = document.querySelector("#page_artists");
+	myNode.addEventListener("click",function(e)
+	{
+		if(e.target.tagName === "IMG")
+		{
+			
+			//Create Overlay
+			var myOverlay = document.createElement("div");
+			myOverlay.id = "overlay";
+			document.body.appendChild(myOverlay);
+
+			//Set up overlay styles
+			myOverlay.style.position = "absolute";
+			myOverlay.style.top = 0;
+			myOverlay.style.backgroundColor = "rgba(0,0,0,0.7)";
+			myOverlay.style.cursor = "pointer";
+
+			//Resize and Reposition overlay
+			myOverlay.style.width = window.innerWidth + "px";
+			myOverlay.style.height = window.innerHeight + "px";
+			myOverlay.style.top = window.pageYOffset + "px";
+			myOverlay.style.left = window.pageXOffset + "px";
+
+			//Add the image inside the overlay
+			var imgSrc = e.target.src;
+			var largeImage = document.createElement("img");
+			largeImage.id = "largeImage";
+			largeImage.src = imgSrc.substr(0,imgSrc.length-7) + ".jpg";
+			largeImage.style.display = "block";
+			largeImage.style.position = "absolute";
+
+			//Wait until the image has loaded
+			largeImage.addEventListener("load",function()
+			{
+				//Resize if taller
+				if(this.height > window.innerHeight)
+				{
+					this.ratio = window.innerHeight/this.height;
+					this.height = this.height*this.ratio;
+					this.width = this.width*this.ratio;
+				}
+
+				//Resize if wider
+				if(this.width > window.innerWidth)
+				{
+					this.ratio = window.innerWidth/this.width;
+					this.height = this.height*this.ratio;
+					this.width = this.width*this.ratio;
+				}
+
+				centerImage(largeImage);
+				myOverlay.appendChild(largeImage);
+			});
+
+			 //Listen for image click
+			largeImage.addEventListener("click",function()
+			{
+				if(myOverlay)
+				{
+					myOverlay.parentNode.removeChild(myOverlay);
+				} //overlay exists
+			},false);
+
+			 //Listen for scroll
+			window.addEventListener("scroll",function()
+			{
+				if(myOverlay)
+				{
+					myOverlay.style.top = window.pageYOffset+"px";
+					myOverlay.style.left = window.pageXOffset+"px";
+				}//overlay exists
+			},false);
+
+		}
+		
+	},false);
+
+	
+
+	function centerImage(theImage)
+	{
+		var DiffX = (window.innerWidth - theImage.width)/2;
+		var DiffY = (window.innerHeight - theImage.height)/2;
+
+		theImage.style.top = DiffY + "px";
+		theImage.style.left = DiffX + "px";
+
+		return theImage;
+	}
+}());
---
+++ 8.Adjusting_For_Scrolling/description.md
@@ -0,0 +1,2 @@
+* When we scroll the window with the overlay open , the overlay remains fixed in position and doesn't move with the window thus creating weird situation.
+* We add an event listener for scroll event to add this functionality.
